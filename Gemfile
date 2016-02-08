3ec6d183dcd4c07a128c967c5badc10977f385ea
SubhashShah-BTC <subhash.shah@botreeconsulting.com>
2016-02-08 19:18:43 +0530

    Added database cleaner to truncate database on every test start

--- Gemfile
+++ Gemfile
@@ -45,6 +45,11 @@ group :development, :test do
   gem 'pry-byebug'
 end
 
+group :test do
+  # Clean up database before every test
+  gem 'database_cleaner'
+end
+
 group :development do
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,15 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'database_cleaner'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  ActiveRecord::Migration.check_pending!
+  DatabaseCleaner.strategy = :truncation
+  setup { DatabaseCleaner.start }
+  teardown { DatabaseCleaner.clean }
 end
